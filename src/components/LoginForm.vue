badef4558f13861bff706aeba186cf4f9ff27a32
yungukbae <yungukbae@gmail.com>
2022-09-01 18:16:59 +0900

    feat: vuex

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -38,11 +38,11 @@ export default {
       try {
         const body = { username: this.username, password: this.password };
         const { data } = await loginUser(body);
+        this.logMessage = `${data.user.username} 님 환영합니다.`;
         if (data.success) {
+          this.$store.commit('setUserName', data.user.username);
           this.$router.push('/main');
         }
-        console.log(data);
-        this.logMessage = `${data.user.username} 님 환영합니다.`;
       } catch (e) {
         console.log(e.response.data);
       } finally {
--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>
-    <header>
-      <router-link to="/login">로그인</router-link> |
-      <router-link to="/signup">회원가입</router-link>
-    </header>
-  </div>
+  <header>
+    <div>
+      <router-link to="/" class="logo"> TIL </router-link>
+    </div>
+    <div>
+      <template v-if="isLogin">
+        <p>{{ $store.state.username }}</p>
+        <a href="javascript:;" @click="logoutUser"></a>
+      </template>
+
+      <template v-else>
+        <router-link to="/login">로그인</router-link> |
+        <router-link to="/signup">회원가입</router-link>
+      </template>
+    </div>
+  </header>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    isLogin() {
+      return this.$store.getters.isLogin;
+    },
+  },
+  methods: {
+    logoutUser() {
+      this.$state.commit('clearUserName');
+      this.$router.push('/');
+    },
+  },
+};
 </script>
 
 <style></style>
